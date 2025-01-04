69ab1f4c6206e6ad01b2782775f34150de7595d8
CalvinKreidy <137772954+CalvinKreidy@users.noreply.github.com>
2025-01-04 16:46:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,19 @@
-/*Setting the scroll bar index width.
-Note: When changing the amount of photos present in the gallery, minor changes in some equations have to be changed to appropriately adapt to the change.*/
-    let DivisionGallery = document.getElementById("DivisionGallery");
-    let DivisionGalleryWidth = DivisionGallery.offsetWidth - 5;
-    let PhotoWidth = DivisionGalleryWidth * 0.35;
-    let TotalPhotoWidth = PhotoWidth * 6;
-
-    let WindowHeight = window.innerHeight;
-    let GapWidth = WindowHeight * 0.03;
-    let TotalGapWidth = GapWidth * 5;
-
-    let TotalGalleryWidth = TotalGapWidth + TotalPhotoWidth;
-    let ScreenGalleryWidth = document.getElementById("DivisionGallery").offsetWidth;
-    let GallerySeenToFullRatio = ScreenGalleryWidth / TotalGalleryWidth;
-
-    let ScrollBarHousing = document.getElementById("ScrollBarHousing").offsetWidth;
-    let ScrollBarIndexSize = ScrollBarHousing * GallerySeenToFullRatio;
-    document.getElementById("ScrollBarIndex").style.width = ScrollBarIndexSize + "px";
-   
-
-//Setting the scroll bar index's position with respect to the content.
-
-     let ScrollableWidth = (TotalGalleryWidth - ScreenGalleryWidth) + 5;
-     setInterval(() => {
-        var ScrollBarIndexPosition = (DivisionGallery.scrollLeft / ScrollableWidth) * (ScrollBarHousing - ScrollBarIndexSize);
-        var ScrollBarIndex = document.getElementById("ScrollBarIndex");
+//Finding the proportions that will help in setting the scroll bar index's width.
+    //Measuring elements and proportions related to 'Division Gallery'.
+    setInterval(() => {
+        let DivisionGallery = document.getElementById("DivisionGallery");
+        let DivisionGalleryScrolledWidth = DivisionGallery.scrollLeft;
+        let DivisionGalleryTotalWidth = DivisionGallery.scrollWidth;
+        let DivisionGalleryScreenWidth = DivisionGallery.getBoundingClientRect().width - 5;
+        let DivisionGalleryScrollableWidth = DivisionGalleryTotalWidth - DivisionGalleryScreenWidth;
+        let DivisionGallery_VisibleToFull_Ratio = DivisionGalleryScreenWidth / DivisionGalleryTotalWidth; 
+        let DivisionGallery_LeftScrolledToScrollableWidth_Ratio = DivisionGalleryScrolledWidth / DivisionGalleryScrollableWidth; 
+        
+        let ScrollBarIndex = document.getElementById("ScrollBarIndex");
+        let ScrollBarHousing = document.getElementById("ScrollBarHousing").getBoundingClientRect().width - 13.5;
+        let ScrollBarIndexSize = ScrollBarIndex.style.width = (ScrollBarHousing * DivisionGallery_VisibleToFull_Ratio) + "px";
+        let ScrollBarIndexSizeRaw = ScrollBarIndex.style.width = ScrollBarHousing * DivisionGallery_VisibleToFull_Ratio;
+        let ScrollBarScrollableWidth = ScrollBarHousing - ScrollBarIndexSizeRaw;
+        let ScrollBarIndexPosition = ScrollBarScrollableWidth * DivisionGallery_LeftScrolledToScrollableWidth_Ratio;
         ScrollBarIndex.style.transform = "translatex(" + ScrollBarIndexPosition + "px)";
-     }, 0);
-   
+     }, );
